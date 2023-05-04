b196318a2a07272fd645f17783635b6abb6d008c
panda <panda@sysfun.ai>
2023-05-04 22:00:17 +0800

    bugfix: remove close when not connected

--- redis.go
+++ redis.go
@@ -45,9 +45,6 @@ func (r *Redis) Client(args ...string) *redis.Client {
 		r.cli = r.Open(args...)
 	})
 	if !TestConnection(r.cli) {
-		if r.cli != nil {
-			_ = r.cli.Close()
-		}
 		r.cli = r.Open(args...)
 	}
 	return r.cli
